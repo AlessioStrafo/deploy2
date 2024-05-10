f39e42977bad8b6347c777ffc009925c605e5872
AlessioStrafo <straforini.alessio@gmail.com>
2024-05-10 12:53:57 +0200

    change version into pom.xml

--- src/main/java/co/develhope/deploy2/ControllerBasic.java
+++ src/main/java/co/develhope/deploy2/ControllerBasic.java
@@ -2,7 +2,15 @@ package co.develhope.deploy2;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//write a Spring Boot application with the necessary dependencies that:
+//has a basic controller that returns the sum of 2 random integers
+//runs on:
+//port 7070 with profile prod
+//port 4000 with profile test
+//port 5000 in all the other cases
+//the version is 0.0.6
+//create the .war and launch it using the command line, using the 2 prod and test profiles:
+//java -jar [activeprofile] [packagename]
 import java.util.Random;
 
 @RestController
